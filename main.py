49ce5b81b2d2c2e41865adad110c20e8e5812ce0
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-17 21:43:43 +0900

    Show list of tags

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 import argparse
-# import querier
-
-# curl https://auth.docker.io/token?service=registry.docker.io&scope=repository:weseek/growi:pull
-# curl -i -H "Authorization: Bearer $TOKEN" https://registry-1.docker.io/v2/weseek/growi/tags/list
+import querier 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Get tags from docker repository')
     parser.add_argument('name', help="name of a docker image including repository name")
     parser.add_argument('-r', '--registry', default="registry-1.docker.io", help="Host name of the registry")
     args = parser.parse_args()
-    print(args)
-    # tags = querier.listTags("weseek/growi")
-    # print(list(tags))
+    tags = querier.list_tags(args.registry, args.name)
+    for tag in tags:
+        print(tag)
--- querier.py
+++ querier.py
@@ -1,19 +1,43 @@
 import requests
 import json
 
-def list_tags(host, name):
-"""
-List the list of tags for a repository from the newer versions
+def get_auth_info(host):
+    r = requests.get(f'https://{host}/v2/')
+    www_auth = r.headers['WWW-Authenticate'].split(" ")
+    assert(len(www_auth) == 2)
+    scheme, params_tmp = www_auth
+    params = dict(map(lambda s: s.split("="), params_tmp.replace("\"", "").split(",")))
+    assert("realm" in params)
+    return scheme, params
 
-Parameters
-----------
-host : string
-    Host address of docker registry
-name : string
-    Image name (username/image)
-""" 
-    r = requests.post(f'{host}/v2/repositories/{repository}/tags')
-    if r.status_code != 200:
-        raise RuntimeError("Failed to fetch data")
+def get_token(host, scope):
+    scheme, params = get_auth_info(host)
+    auth_url = f'{params["realm"]}?service={params["service"]}&scope={scope}'
+    r = requests.get(auth_url)
     body = json.loads(r.text)
-    return map(lambda image: image["name"], body["results"])
+    assert("token" in body)
+    return body["token"]
+
+def list_tags(host, name):
+    """
+    List the list of tags for a repository from the newer versions
+    
+    Parameters
+    ----------
+    name : string
+        Image name (username/image)
+    """ 
+
+    scope = f"repository:{name}:pull"
+    token = get_token(host, scope)
+    headers = {
+        'Authorization': f'Bearer {token}'
+    }
+
+    r = requests.get(f'https://{host}/v2/{name}/tags/list', headers=headers)
+    if r.status_code != 200:
+        print(r.text)
+        raise RuntimeError(f'Failed to fetch data. Status code {r.status_code}')
+
+    body = json.loads(r.text)
+    return body["tags"]
