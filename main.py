dfadabcc1dd7688701f2acb0e055fdeec6329680
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-17 21:47:30 +0900

    Remove lint errors

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import argparse
-import querier 
+import querier
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Get tags from docker repository')
-    parser.add_argument('name', help="name of a docker image including repository name")
-    parser.add_argument('-r', '--registry', default="registry-1.docker.io", help="Host name of the registry")
+    parser = argparse.ArgumentParser(
+        description='Get tags from docker repository'
+    )
+    parser.add_argument(
+        'name',
+        help="name of a docker image including repository name"
+    )
+    parser.add_argument(
+        '-r', '--registry',
+        default="registry-1.docker.io",
+        help="Host name of the registry"
+    )
+
     args = parser.parse_args()
     tags = querier.list_tags(args.registry, args.name)
     for tag in tags:
--- querier.py
+++ querier.py
@@ -1,14 +1,18 @@
 import requests
 import json
 
+
 def get_auth_info(host):
     r = requests.get(f'https://{host}/v2/')
     www_auth = r.headers['WWW-Authenticate'].split(" ")
     assert(len(www_auth) == 2)
     scheme, params_tmp = www_auth
-    params = dict(map(lambda s: s.split("="), params_tmp.replace("\"", "").split(",")))
+    params = dict(
+        map(lambda s: s.split("="), params_tmp.replace("\"", "").split(","))
+    )
     assert("realm" in params)
     return scheme, params
+
 
 def get_token(host, scope):
     scheme, params = get_auth_info(host)
@@ -18,15 +22,16 @@ def get_token(host, scope):
     assert("token" in body)
     return body["token"]
 
+
 def list_tags(host, name):
     """
     List the list of tags for a repository from the newer versions
-    
+
     Parameters
     ----------
     name : string
         Image name (username/image)
-    """ 
+    """
 
     scope = f"repository:{name}:pull"
     token = get_token(host, scope)
@@ -37,7 +42,9 @@ def list_tags(host, name):
     r = requests.get(f'https://{host}/v2/{name}/tags/list', headers=headers)
     if r.status_code != 200:
         print(r.text)
-        raise RuntimeError(f'Failed to fetch data. Status code {r.status_code}')
+        raise RuntimeError(
+            f'Failed to fetch data. Status code {r.status_code}'
+        )
 
     body = json.loads(r.text)
     return body["tags"]
