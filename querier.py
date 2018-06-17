10d0e340e09bca816511f4e6e9d734d175230528
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-18 00:01:43 +0900

    Add type checking by mypy

--- querier.py
+++ querier.py
@@ -1,29 +1,33 @@
 import requests
 import json
+from typing import Dict, List, Tuple
 
 
-def get_auth_info(host):
+def get_auth_info(host: str) -> Tuple[str, Dict[str, str]]:
     r = requests.get(f'https://{host}/v2/')
     www_auth = r.headers['WWW-Authenticate'].split(" ")
     assert(len(www_auth) == 2)
     scheme, params_tmp = www_auth
-    params = dict(
-        map(lambda s: s.split("="), params_tmp.replace("\"", "").split(","))
+    params: Dict[str, str] = dict(
+        map(
+            lambda s: (s.split("=")[0], s.split("=")[1]),
+            params_tmp.replace("\"", "").split(",")
+        )
     )
     assert("realm" in params)
-    return scheme, params
+    return scheme, params, 1
 
 
-def get_token(host, scope):
+def get_token(host: str, scope: str) -> str:
     scheme, params = get_auth_info(host)
     auth_url = f'{params["realm"]}?service={params["service"]}&scope={scope}'
     r = requests.get(auth_url)
-    body = json.loads(r.text)
+    body: Dict[str, str] = json.loads(r.text)
     assert("token" in body)
     return body["token"]
 
 
-def list_tags(host, name):
+def list_tags(host: str, name: str) -> List[str]:
     """
     List the list of tags for a repository from the newer versions
 
