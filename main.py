52fb04b7f5213db66b27353cb36606d01845fa6f
Otsuki Hitoshi <hitochan777@gmail.com>
2018-06-19 21:10:29 +0900

    Make tag a subcommand

--- main.py
+++ main.py
@@ -6,16 +6,24 @@ if __name__ == "__main__":
         description='Get tags from docker repository'
     )
     parser.add_argument(
-        'name',
-        help="name of a docker image including repository name"
-    )
-    parser.add_argument(
         '-r', '--registry',
         default="registry-1.docker.io",
         help="Host name of the registry"
     )
+    subparsers = parser.add_subparsers(help='List of subcommands', dest="subcommand_name")
+
+    parser_tag = subparsers.add_parser('tag', help='tag help')
+    parser_tag.add_argument(
+        'name',
+        help="name of a docker image including repository name"
+    )
 
     args = parser.parse_args()
-    tags = querier.list_tags(args.registry, args.name)
-    for tag in tags:
-        print(tag)
+
+    if args.subcommand_name == "tag":
+        tags = querier.list_tags(args.registry, args.name)
+        for tag in tags:
+            print(tag)
+
+    else:
+        raise ValueError(f"Unexpected subcommand {args.subcommand_name}")
